db68397ca6d19c56aef4b6813c32765d46f44ca8
Maurits Brouwer <36409024+Maurits97@users.noreply.github.com>
2021-02-06 15:01:08 +0100

    First publish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import './css/App.css';
+
+import User from './components/User';
+import Account from './components/Account';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/account">Account</Link>
+        </div>
+
+        <p> Counter {counter}</p>
+        <button onClick={() => dispatch(increment(5))}>+</button>
+        <button onClick={() => dispatch(decrement())}>+</button>
+        {isLogged ? <p>Information i shouldnt see</p>: ''}
+
+        <hr/>
+        <Route exact path="/" component={User}/>
+        <Route exact path="/account" component={Account}/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Info from './Info'
+
+export default function (props) {
+    return (
+        <div>
+            <h1>User page</h1>
+            <Info />
+        </div>
+        
+    );
+}
