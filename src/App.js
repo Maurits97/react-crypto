f8d211d882f4598e83e11310223f34ba5d7f6b91
Maurits Brouwer <36409024+Maurits97@users.noreply.github.com>
2021-02-24 10:43:06 +0100

    added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as HashRouter, Route} from 'react-router-dom'
 
 import NavContainer from './components/NavContainer'
 import FooterContainer from './components/FooterContainer'
@@ -11,7 +11,7 @@ import MarketCapContainer from './components/MarketCapContainer';
 function App() {
   return (
     <Provider store={store}>
-      <Router>
+      <HashRouter basename="/#">
           <NavContainer />
 
           <Route exact path="/" component={MarketCapContainer}/>
@@ -19,7 +19,7 @@ function App() {
 
           <FooterContainer />
           
-      </Router>
+      </HashRouter>
     </Provider>
   );
 }
